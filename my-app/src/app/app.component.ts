10f6a69d055536e7c5bb55e3c3ef352e47eb8a27
MSraoSuryawanshi <mnjptl20@gmail.com>
2021-10-29 08:30:09 +0530

    bootstrap

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 // app.component.ts start
 @Component({
   selector: 'app-start', // <app-start></app-start>
-  template: '<h1>Hello Angular</h1>', // UI
+  templateUrl: './app.component.html', // UI
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Hello Angular';
+  products = ['Mobiles', 'Laptops', 'Books'];
+}
 // app.component.ts end
+
+// document.write(title) = {{title}}
+// {{ var / 2 + 4 }} interpollation ( Data Binding )
