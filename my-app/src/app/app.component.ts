cbdc6e72964c49633cb70d36143027abe35726b9
MSraoSuryawanshi <mnjptl20@gmail.com>
2021-11-01 08:32:32 +0530

    prop binding

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -9,9 +9,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello Angular';
   products = [
-    { id: 1001, name: 'mobile', price: 1200, inStock: 80 },
-    { id: 1002, name: 'Laptop', price: 1500, inStock: 0 },
-    { id: 1003, name: 'BOOKS', price: 1800, inStock: 10 },
+    {
+      id: 1001,
+      name: 'mobile',
+      price: 1200,
+      inStock: 80,
+      textColor: 'red',
+      isNew: false,
+    },
+    {
+      id: 1002,
+      name: 'Laptop',
+      price: 1500,
+      inStock: 0,
+      textColor: 'green',
+      isNew: true,
+    },
+    {
+      id: 1003,
+      name: 'BOOKS',
+      price: 1800,
+      inStock: 10,
+      textColor: 'blue',
+      isNew: false,
+    },
   ];
 }
 // app.component.ts end
